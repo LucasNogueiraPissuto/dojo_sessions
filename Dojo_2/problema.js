6fe63de7f70e9691372a5379c314b9ee9a12bca7
hamiltonssj4 <hamiltonssj4@gmail.com>
2024-04-04 17:54:56 -0300

    Co-authored-by: Lucas Nogueira Pissuto <lucasnogueirapissuto@gmail.com> Co-authored-by: enfalineaferreira@hotmail.com <enfalineaferreira@hotmail.com> Co-authored-by: sxaoliveira13 <sxaoliveira13@users.noreply.github.com> Co-authored-by: Natalia Quirino De Oliveira <NatiOli@users.noreply.github.com> Co-authored-by: Ana Carolina Tolêdo <anacarolinatoledo@users.noreply.github.com> Co-authored-by: laryscout <laryscout@users.noreply.github.com>

---
+++ Dojo_2/problema.js
@@ -0,0 +1,28 @@
+/* Converter string em caixa de camelo 
+Conclua o método/função para que ele converta palavras delimitadas por traço/sublinhado em camel case . A primeira palavra na saída deve ser maiúscula apenas se a palavra original estiver em maiúscula (conhecida como Upper Camel Case, também conhecida como Pascal case). As próximas palavras devem ser sempre maiúsculas.
+
+Exemplos
+"thes-tealth-warrior"é convertido para"theStealthWarrior"
+
+"The_Stealth_Warrior"é convertido para"TheStealthWarrior"
+
+"The_Stealth-Warrior"é convertido para"TheStealthWarrior"
+
+*/
+/*variavel que contem a string inteira*/
+StringInteira = "the-stealth-warrior";
+
+function noCaractere(StringInteira){
+    return StringInteira.replace()
+}
+
+console.log(noCaracter)
+
+
+
+
+
+
+
+
+//.toUpperCase
